9fa2ca621ca418dd6618b3b6c0329593bc186a4a
ogabek0423 <ogabek0423@gmail.com>
2024-06-17 22:10:29 +0500

    fifth

--- django_web_app/users/views.py
+++ django_web_app/users/views.py
@@ -5,6 +5,7 @@ from .forms import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
 
 
 class UserInfoView(View):
